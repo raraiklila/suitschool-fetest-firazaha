119ecf4bd22b73177bb4df6acfc40ce73be00eb4
raraiklila <fira.ndul146@gmail.com>
2026-01-11 17:36:20 +0700

    bismillah lolos

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://suitmedia-backend.suitdev.com',
+      "/api": {
+        target: "https://suitmedia-backend.suitdev.com",
         changeOrigin: true,
-        secure: false,
-      },
-    },
+        secure: false
+      }
+    }
   },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        work: resolve(__dirname, "work.html"),
+        about: resolve(__dirname, "about.html"),
+        services: resolve(__dirname, "services.html"),
+        careers: resolve(__dirname, "careers.html"),
+        contact: resolve(__dirname, "contact.html")
+      }
+    }
+  }
 });
