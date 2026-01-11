bc48855984281001b08e4eb9d0af27799ebe6f1e
raraiklila <fira.ndul146@gmail.com>
2026-01-11 17:20:43 +0700

    bismillah ya Allah

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html'),
-        about: resolve(__dirname, 'public/about.html'),
-        work: resolve(__dirname, 'public/work.html'),
-        services: resolve(__dirname, 'public/services.html'),
-        careers: resolve(__dirname, 'public/careers.html'),
-        contact: resolve(__dirname, 'public/contact.html'),
-      },
-    },
-  },
   server: {
     proxy: {
       '/api': {
         target: 'https://suitmedia-backend.suitdev.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        secure: false,
       },
     },
   },
